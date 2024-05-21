7fed926501b50a55399f04ed933254504533213e
hdsong2 <holden_s@163.com>
2024-05-21 11:02:53 +0800

    feat(user): generate mongodb cmd by excel

--- whitelist.py
+++ whitelist.py
@@ -3,18 +3,36 @@
 import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
 
-doc = 'db.user_whitelist.inMany()'
+import json
+from datetime import datetime
 
-def generateDoc(sheet: Worksheet, account: str, module: str, enabled: str, start_time: str, end_time: str) -> str:
-	doc = {
-		'account': account,
-		'type': module,
-		
-	}
+cmd = 'db.user_whitelist.insertMany({})'
+
+def string_to_timestamp(date_string, format="%Y-%m-%d %H:%M:%S"):
+    dt_object = datetime.strptime(date_string, format)  
+    timestamp = dt_object.timestamp()  
+    return timestamp  
+
+def generateDocs(sheet: Worksheet) -> str:
+    docs = []
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        (account, cloudType, enabled, startTime, endTime) = row
+        doc = {
+            'account': account,
+            'type': cloudType,
+            'enabled': bool(enabled),
+            'start_time': int(startTime.timestamp()),
+            'end_time': int(endTime.timestamp()),
+        }
+        docs.append(doc)
+
+    return json.dumps(docs)
 
 if __name__ == '__main__':
-    workbook = openpyxl.load_workbook('whitelist.xlsx')
+    workbook = openpyxl.load_workbook('./whitelist.xlsx')
     sheet = workbook.active
-    # generate db command
-    workbook.close() 
+    docs_string = generateDocs(sheet)
+    workbook.close()
+    with open('./cmd.js', 'w') as f:
+        f.write(cmd.format(docs_string))
     
