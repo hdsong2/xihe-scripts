b4ad6c5fe8b345754e46398464e507206e317466
hdsong2 <holden_s@163.com>
2024-07-22 17:04:59 +0800

    feat(promotion): add promotion generator

--- promotion_generator.py
+++ promotion_generator.py
@@ -1,40 +1,66 @@
 # -*- encoding: utf-8 -*-
 
-import uuid
 import json
-from datetime import datetime, timedelta
 import argparse
 import sys
+from datetime import timezone
 
-def generate_promotion_info(promotion_id="", name="", desc="", duration="", poster="") -> str:
-    if promotion_id == "":
-        promotion_id = "mindSpore_competition_{}".format(str(uuid.uuid4())[:6])
+import openpyxl
+from openpyxl.worksheet.worksheet import Worksheet
 
-    if duration == "":
-        start = datetime.now()
-        end = start + timedelta(days=7)
-        duration = datetime.strftime(start, '%Y.%m.%d') + '-' + datetime.strftime(end, '%Y.%m.%d')
+import utils
+
+cmd = 'db.promotion.insertMany({}, {{ordered: false}})'
+
+def generate_promotion_info(sheet: Worksheet) -> str:
+    docs = []
+    for row in sheet.iter_rows(min_row=sheet.min_row+1, max_row=sheet.max_row, values_only=True):
+        (promotion_id, name, desc, startTime, endTime, host, kind, way, priority, poster, intro) = row
         
-    return json.dumps({
-        'id': promotion_id,
-        'name': name,
-        'desc': desc,
-        'reg_users': [],
-        'duration': duration,
-        'version': 1,
-        'poster': poster,
-    })
+        if promotion_id == None:
+            continue
+
+        if isinstance(startTime, str):
+            startTime = utils.str_to_datetime(startTime)
+        if isinstance(endTime, str):
+            endTime = utils.str_to_datetime(endTime)
+            
+        doc = {
+            'id': promotion_id,
+            'name': name,
+            'desc': desc,
+            'start_time': int(startTime.timestamp()),
+            'end_time': int(endTime.timestamp()),
+            'type': kind,
+            'host': host if host else "",
+            'way': way,
+            'poster': poster,
+            'intro': intro,
+        }
+
+        if priority != None:
+            doc['priority'] = priority
+        
+        docs.append(doc)
+
+    return json.dumps(docs)
+
+def main(args):
+    workbook = openpyxl.load_workbook(args.filename)
+    sheet = workbook.active
+    info = generate_promotion_info(sheet)
+    workbook.close()
+
+    out = cmd.format(info)
+
+    sys.stdout.write('\n' + out + '\n\n')
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pid', required=True, help='promotion id')
-    parser.add_argument('-n', '--name', required=True, help='promotion name')
-    parser.add_argument('--desc', required=True, help='promotion desc')
-    parser.add_argument('-d', '--duration', required=True, help='promotion duration')
+    
+    parser.add_argument('-f', '--filename', required=True, help='the excel of promotion')
 
     args = parser.parse_args()
 
-    out = 'db.promotion.insert({})'.format(generate_promotion_info(promotion_id= args.pid, name=args.name, 
-                                                                   desc=args.desc, duration=args.duration))
-    
-    sys.stdout.write('\n' + out + '\n\n')
+    main(args)
