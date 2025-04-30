17ccab5df08d05d21a7328d8f012fd8472d8c46a
hdsong2 <holden_s@163.com>
2025-04-30 17:49:16 +0800

    feat(mongo): add support for optional TLS on environment

--- mongo.py
+++ mongo.py
@@ -9,24 +9,43 @@ from pymongo.database import Database
 _client = None
 _database = None
 
+
 def initialize(config: Dict[str, Union[str, int]]):
-    (rwuser, password, ip, port, database, ca_file) = \
-    config['rwuser'], config['password'], config['ip'], config['port'], config['database'], config["cafile"]
+    (rwuser, password, ip, port, database, ca_file) = (
+        config["rwuser"],
+        config["password"],
+        config["ip"],
+        config["port"],
+        config["database"],
+        config["cafile"],
+    )
 
     global _client, _database
     user = urllib.parse.quote_plus(rwuser)
     pwd = urllib.parse.quote_plus(password)
 
-    uri = 'mongodb://{user}:{password}@{ip}:{port}/?authSource={db}'.format(
-        user=user, password=pwd, ip=ip, port=port, db=database)
-    _client = MongoClient(uri, connectTimeoutMS=5000, tls=True, tlsAllowInvalidHostnames=True, tlsCAFile=ca_file)
+    uri = "mongodb://{user}:{password}@{ip}:{port}/?authSource={db}".format(
+        user=user, password=pwd, ip=ip, port=port, db=database
+    )
+    ca_file = ca_file if ca_file != "" else None
+    tls = False if ca_file is None else True
+
+    _client = MongoClient(
+        uri,
+        connectTimeoutMS=5000,
+        tls=tls,
+        tlsAllowInvalidHostnames=True,
+        tlsCAFile=ca_file,
+    )
     _database = database
+
 
 def db(name: str = None) -> Database:
     if _client == None or _database == None:
-        raise Exception('please call mongo.initialize first')
+        raise Exception("please call mongo.initialize first")
 
     return _client[name if name else _database]
+
 
 def close():
     if _client != None:
